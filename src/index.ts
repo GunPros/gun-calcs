e2a59d574d48b65044502ac7aeb0ddef40c2f4e3
DoUWant2DevApp <93307121+DoUWant2DevApp@users.noreply.github.com>
2022-10-17 14:28:22 -0400

    add exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import kineticEnergy from "./Functions/kineticEnergy"
+import type {
+  kineticEnergyParamsType,
+  kineticEnergyReturnType,
+} from "./Functions/kineticEnergyTypes"
 import { ftLbsToJoules } from "./Functions/UnitsConversion"
+import type { ballisticTrajectoryParamsType } from "./Functions/ballisticTrajectory"
 
-export { kineticEnergy, ftLbsToJoules }
+export {
+  kineticEnergy,
+  kineticEnergyParamsType,
+  kineticEnergyReturnType,
+  ftLbsToJoules,
+  ballisticTrajectoryParamsType,
+}
