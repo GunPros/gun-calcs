1eddb8cf4cb2562ff42026a2237069491b5c93f9
DoUWant2DevApp <93307121+DoUWant2DevApp@users.noreply.github.com>
2022-10-17 02:36:43 -0400

    init

--- src/Functions/CalculateRecoilEnergy.ts
+++
@@ -1,7 +0,0 @@
-export default function CalculateRecoilEnergy(
-  bulletWeightGrains: number,
-  muzzleVelocityFps: number,
-  powderChargeWeightGrains: number,
-  firearmWeightLbs: number,
-  firearmType: "Handgun" | "Rifle" | "Average Shotgun" | "Long Shotgun",
-) {}
